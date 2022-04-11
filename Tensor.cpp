d6b93c8a8e48d738e0499e870cbbcc9443cbb1b9
jshatto0225 <103402189+jshatto0225@users.noreply.github.com>
2022-04-11 00:53:42 -0400

    Update Tensor.cpp

--- Tensor.cpp
+++ Tensor.cpp
@@ -6,15 +6,21 @@ using namespace std;
 class Tensor{
     private:
         int depth;
-        int rows;
-        int columns;
         Matrix* tensor;
     public:
         Tensor(){}
-        Tensor(int d, int r, int c){
-            depth, rows, columns = d, r, c;
+        Tensor(int d, Matrix m[]){
+            depth = d;
+            tensor = m;
         }
-        void setValue(int d, int r, int c){
-            
+        void setValue(int d, int r, int c, float val){
+            tensor[d].setVal(r, c, val);
+        }
+        float getVal(int d, int r, int c){
+            return tensor[d].getVal(r, c);
         }
 };
+
+int main(){
+    return 0;
+}
