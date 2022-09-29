08d757428374cb9177ffa9be457dc8576f899a65
jshatto0225 <103402189+jshatto0225@users.noreply.github.com>
2022-09-28 20:47:34 -0400

    Update and rename LinearRegression.cpp to Regression.cpp

--- CPlusPlus/MachineLearning/Regression.cpp
+++ CPlusPlus/MachineLearning/Regression.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
 #include <stdio.h>
 #include "../Matrix/Matrix.cpp"
+#include <cmath>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
 // Creates matrix with dimentions (inputs.cols_ + 1, 1) that correspond to coeficients of regression line
 // out = lr(0, 0)x + lr(1, 0)y + ... + lr(input.cols_+1, 0)
-Matrix lr(Matrix inputs, Matrix outputs) {
+// bool linear set to true will perform regression on linear data
+// bool linear set to false will perform polynomial regression with order inputs.cols_
+// poly regression will be returned in the order ax + bx^2 + cx^3 + ... + C(constant)
+Matrix regress(Matrix inputs, Matrix outputs, bool linear = true) {
     // Creates matrix a with dimentions r, c+1
     Matrix a(inputs.getRows(), inputs.getCols() + 1);
     // Sets a values to input values and sets extra column to ones
     for (int i = 0; i < a.getRows(); i++) {
         for (int j = 0; j < a.getCols(); j++) {
             (j < inputs.getCols()) ? a(i, j) = inputs(i, j) : a(i, j) = 1;
+            // If using poly regression raise element[i][j] to jth power
+            a(i, j) = pow(a(i, j), j + 1) * !linear;
         }
     }
-    // Linear regression formula
+    // Multiple regression formula
     Matrix aT = a.transpose();
     Matrix p = aT * a;
     Matrix pInverse = p.invert();
@@ -26,15 +33,15 @@ Matrix lr(Matrix inputs, Matrix outputs) {
 
 int main() {
 
-    double arr[6] = { 1, 1, 1, 2, 2, 2 };
-    double arr2[3] = { 7, 10, 14 };
+    double arr[12] = { 1, 1, 1, 1, 4, 2, 7, 4, 2 , 3, 8, 2 };
+    double arr2[4] = { 16, 90, 126, 294 };
 
-    Matrix inputs(arr, 3, 2);
-    Matrix outputs(arr2, 3, 1);
+    Matrix inputs(arr, 4, 3);
+    Matrix outputs(arr2, 4, 1);
 
     cout << inputs << endl << outputs << endl;
 
-    Matrix eq = lr(inputs, outputs);
+    Matrix eq = regress(inputs, outputs, false);
 
     cout << eq;
 
